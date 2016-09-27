d1e49ecc2621ab4bcfa003696d02246960f3eb29
Chajunseo <sem03020@naver.com>
2016-09-27 18:53:08 +0900

    Add files via upload

---
+++ Spawnpoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawnpoint : MonoBehaviour {
+	public GameObject debri; //우주쓰레기 프리팹
+	public float interval=1f; //쓰레기 발생간격(시간) 
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("SpawnDebri");//코루틴의 시작 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator SpawnDebri(){
+				while (true) {
+						Instantiate (debri, transform.position, Quaternion.identity);
+						//Instantiate(원본, 좌표, 각도);
+						yield return new WaitForSeconds (interval);
+				}
+		}
+}
+
